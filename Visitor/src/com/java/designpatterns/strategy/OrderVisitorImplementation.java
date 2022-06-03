2abd1b16a050b80946326b8b9fa026b889956319
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2022-06-03 18:24:52 +0200

    Fixed output

--- Visitor/src/com/java/designpatterns/strategy/OrderVisitorImplementation.java
+++ Visitor/src/com/java/designpatterns/strategy/OrderVisitorImplementation.java
@@ -4,14 +4,14 @@ public class OrderVisitorImplementation implements OrderVisitor {
     @Override
     public int visit(Pasta pasta) {
         int price = pasta.getPrice() * pasta.getNumberOfDishes();
-        System.out.println("Pasta: " + pasta.getDetailedDishName() + ", price = " + price+ " for "+pasta.getNumberOfDishes()+ " portions.");
+        System.out.println("Pasta: " + pasta.getDetailedDishName() + ", price = " + price + " for " + pasta.getNumberOfDishes() + " portions.");
         return price;
     }
 
     @Override
     public int visit(Steak steak) {
         int price = steak.getPrice() * steak.getNumberOfDishes();
-        System.out.println("Steak: " + steak.getDetailedDishName() + ", price = " + price+ " for "+steak.getNumberOfDishes()+ " portions.");
+        System.out.println("Steak: " + steak.getDetailedDishName() + ", price = " + price + " for " + steak.getNumberOfDishes() + " portions.");
         return price;
     }
 }
